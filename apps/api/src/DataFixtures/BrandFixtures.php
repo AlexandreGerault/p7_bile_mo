085e0964b857c61da72cac88024a44ee1ba6e8a9
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-10-23 11:02:01 +0200

    fix: codeclimate issues

--- apps/api/src/DataFixtures/BrandFixtures.php
+++ apps/api/src/DataFixtures/BrandFixtures.php
@@ -20,41 +20,76 @@ class BrandFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+        $this->registerApple($manager);
+
+        $this->registerSamsung($manager);
+
+        $this->registerXiaomi($manager);
+
+        $this->registerHuawei($manager);
+
+        $this->registerSony($manager);
+
+        $this->registerLg($manager);
+
+        $this->registerMotorola($manager);
+
+        $manager->flush();
+    }
+
+    private function registerApple(ObjectManager $manager): void
+    {
         $appleBrand = new Brand();
         $appleBrand->setName('Apple');
         $manager->persist($appleBrand);
         $this->addReference(self::APPLE_BRAND_REFERENCE, $appleBrand);
+    }
 
+    private function registerSamsung(ObjectManager $manager): void
+    {
         $samsungBrand = new Brand();
         $samsungBrand->setName('Samsung');
         $manager->persist($samsungBrand);
         $this->addReference(self::SAMSUNG_BRAND_REFERENCE, $samsungBrand);
+    }
 
+    private function registerXiaomi(ObjectManager $manager): void
+    {
         $xiaomiBrand = new Brand();
         $xiaomiBrand->setName('Xiaomi');
         $manager->persist($xiaomiBrand);
         $this->addReference(self::XIAOMI_BRAND_REFERENCE, $xiaomiBrand);
+    }
 
+    private function registerHuawei(ObjectManager $manager): void
+    {
         $huaweiBrand = new Brand();
         $huaweiBrand->setName('Huawei');
         $manager->persist($huaweiBrand);
         $this->addReference(self::HUAWEI_BRAND_REFERENCE, $huaweiBrand);
+    }
 
+    private function registerSony(ObjectManager $manager): void
+    {
         $sonyBrand = new Brand();
         $sonyBrand->setName('Sony');
         $manager->persist($sonyBrand);
         $this->addReference(self::SONY_BRAND_REFERENCE, $sonyBrand);
+    }
 
+    private function registerLg(ObjectManager $manager): void
+    {
         $lgBrand = new Brand();
         $lgBrand->setName('LG');
         $manager->persist($lgBrand);
         $this->addReference(self::LG_BRAND_REFERENCE, $lgBrand);
-
+    }
+    
+    private function registerMotorola(ObjectManager $manager): void
+    {
         $motorolaBrand = new Brand();
         $motorolaBrand->setName('Motorola');
         $manager->persist($motorolaBrand);
         $this->addReference(self::MOTOROLA_BRAND_REFERENCE, $motorolaBrand);
-
-        $manager->flush();
     }
 }
--- apps/api/src/DataFixtures/PhoneFixtures.php
+++ apps/api/src/DataFixtures/PhoneFixtures.php
@@ -15,81 +15,44 @@ class PhoneFixtures extends Fixture
     {
         /** @var Brand $apple */
         $apple = $this->getReference(BrandFixtures::APPLE_BRAND_REFERENCE);
-        for ($i = 7; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('iPhone ' . $i);
-            $phone->setDescription('iPhone ' . $i . ' description');
-            $phone->setBrand($apple);
-            $phone->setPrice(100000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($apple, 'iPhone', 1000 * 100, $manager);
 
         /** @var Brand $samsung */
         $samsung = $this->getReference(BrandFixtures::SAMSUNG_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('Galaxy ' . $i);
-            $phone->setDescription('Galaxy ' . $i . ' description');
-            $phone->setBrand($samsung);
-            $phone->setPrice(80000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($samsung, 'Galaxy', 800 * 100, $manager);
 
         /** @var Brand $xiaomi */
         $xiaomi = $this->getReference(BrandFixtures::XIAOMI_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('Mi ' . $i);
-            $phone->setDescription('Mi ' . $i . ' description');
-            $phone->setBrand($xiaomi);
-            $phone->setPrice(70000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($xiaomi, 'Mi', 400 * 100, $manager);
 
         /** @var Brand $huawei */
         $huawei = $this->getReference(BrandFixtures::HUAWEI_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('P ' . $i);
-            $phone->setDescription('P ' . $i . ' description');
-            $phone->setBrand($huawei);
-            $phone->setPrice(55000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($huawei, 'P', 550 * 100, $manager);
 
         /** @var Brand $sony */
         $sony = $this->getReference(BrandFixtures::SONY_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('Xperia ' . $i);
-            $phone->setDescription('Xperia ' . $i . ' description');
-            $phone->setBrand($sony);
-            $phone->setPrice(65000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($sony, 'Xperia', 450 * 100, $manager);
 
         /** @var Brand $lg */
         $lg = $this->getReference(BrandFixtures::LG_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('G ' . $i);
-            $phone->setDescription('G ' . $i . ' description');
-            $phone->setBrand($lg);
-            $phone->setPrice(75000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($lg, 'G', 700 * 100, $manager);
 
         /** @var Brand $motorola */
         $motorola = $this->getReference(BrandFixtures::MOTOROLA_BRAND_REFERENCE);
-        for ($i = 1; $i < 15; $i++) {
-            $phone = new Phone();
-            $phone->setModel('Moto ' . $i);
-            $phone->setDescription('Moto ' . $i . ' description');
-            $phone->setBrand($motorola);
-            $phone->setPrice(85000 + $i * 10000);
-            $manager->persist($phone);
-        }
+        $this->registerPhonesForBrand($motorola, 'Moto', 650 * 100, $manager);
 
         $manager->flush();
+    }
+
+    private function registerPhonesForBrand(Brand $brand, string $model, int $basePrice, ObjectManager $manager): void
+    {
+        for ($i = 7; $i < 15; $i++) {
+            $phone = new Phone();
+            $phone->setModel($model . ' ' . $i);
+            $phone->setDescription($model . ' ' . $i . ' description');
+            $phone->setBrand($brand);
+            $phone->setPrice($basePrice + $i * 10000);
+            $manager->persist($phone);
+        }
     }
 }
